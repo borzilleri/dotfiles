c098ff62ddb9eb03bddc1028a6ae7143e2fdc04a
Jonathan Borzilleri <jborzilleri@intrpeidls.com>
2012-01-03 10:51:50 -0800

    Fixes machine-specific file loading
    - rename "machine_filse" dir to "machine"
    - Add machine file for sdf3.
    - Properly add .bash to node names

---
+++ bash/machine/sdf3.bash
@@ -0,0 +1,11 @@
+export JAVA_HOME='/home/jborzilleri/Applications/jdk1.6.0_24';
+export M2_HOME='/home/jborzilleri/Applications/apache-maven-2.2.1';
+export CATALINA_HOME='/home/jborzilleri/Applications/apache-tomcat-6.0.26';
+
+export ANT_HOME='/home/jborzilleri/Applications/apache-ant-1.8.2';
+
+PATH="$ANT_HOME/bin:$M2_HOME/bin:$JAVA_HOME/bin:$PATH";
+PATH="/var/lib/gems/1.8/bin:$PATH";
+
+alias ']'='gnome-open';
+alias 'docblox'="php $HOME/src/Docblox/bin/docblox.php";
