158283a630e96ca5cdddebc4578b196a69cb0986
Jonathan Borzilleri <jborzilleri@intrepidls.com>
2012-09-23 17:56:18 -0700

    Adds hushlogin, gitconfig, updates sdf-3.local, gitignore, inputrc
    - Adds hushlogin bash fioel
    - Adds gitconfig file
    - Updates gitignore file
    - Reorganizing inputrc file.
    - Adds some settings to inputrc.
    - Updates sdf-3.local bash file with correct JAVA_HOME, MVN_HOME, and
    	PLAY2_HOME environment variables

--- bash/machine/sdf-3.local.bash
+++ bash/machine/sdf-3.local.bash
@@ -1,7 +1,10 @@
 export GEM_HOME='/Users/jborzilleri/.gems/1.8'
 export GEM_PATH='/Users/jborzilleri/.gems/1.8'
-export M2="/usr/local/Cellar/maven/3.0.1/libexec/bin"
-export M2_HOME="/usr/local/Cellar/maven/3.0.1/libexec"
+export JAVA_HOME=$(/usr/libexec/java_home)
+
+export MVN_HOME="/usr/local/Cellar/maven/3.0.1/libexec"
+export PLAY2_HOME="/usr/local/Cellar/play/2.0.3/libexec"
+
 
 PATH="$GEM_PATH/bin:$PATH";
 
