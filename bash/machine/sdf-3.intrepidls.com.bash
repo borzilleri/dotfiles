554324416f5cf24e544766f9d39605c984c2e709
Jonathan Borzilleri <jborzilleri@intrepidls.com>
2015-02-23 12:14:41 -0800

    Export DOCKER_HOST on sdf-3

--- bash/machine/sdf-3.intrepidls.com.bash
+++ bash/machine/sdf-3.intrepidls.com.bash
@@ -2,7 +2,8 @@ export GEM_PATH="/Users/jborzilleri/.gems/1.8"
 export GEM_HOME="$GEM_PATH"
 export JAVA_HOME=$(/usr/libexec/java_home)
 export MVN_HOME="$(brew --prefix maven)/libexec"
-export GOPATH="$HOME/src/go"
+export GOPATH="$HOME/src/go:$HOME/Projects/go"
+export DOCKER_HOST="tcp://docker.intrepid.agile:2375"
 
 PATH="$GEM_PATH/bin:$PATH";
 
