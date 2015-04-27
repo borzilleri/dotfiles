7edffa6e24e6ad1a57c360a672be7b6008a08bd4
Jonathan Borzilleri <jon.borzilleri@intrepidlearning.com>
2015-04-27 11:52:04 -0700

    Adds sdf-1 bash file.
    Signed-off-by: Jonathan Borzilleri <jon.borzilleri@intrepidlearning.com>

---
+++ bash/machine/sdf-1.local.bash
@@ -0,0 +1,12 @@
+export GEM_PATH="/Users/jborzilleri/.gems/1.8"
+export GEM_HOME="$GEM_PATH"
+export JAVA_HOME=$(/usr/libexec/java_home)
+export MVN_HOME="$(brew --prefix maven)/libexec"
+export GOPATH="$HOME/src/go:$HOME/Projects/go"
+export DOCKER_HOST="tcp://docker.intrepid.agile:2375"
+
+#PATH="$GEM_PATH/bin:$PATH";
+
+PS1="\h:\W${GREEN}\$(__git_ps1 ':%s')${RESET}\$ ";
+
+#complete -C aws_completer aws
