94d8f8f8b91146bedb3524ceb2936feee9429fa7
Jonathan Borzilleri <jonathan@borzilleri.net>
2016-02-27 14:22:31 -0800

    Update GOPATH, DOCKER env vars on terminaldogma

--- bash/machine/terminaldogma.local.bash
+++ bash/machine/terminaldogma.local.bash
@@ -1,4 +1,5 @@
 PATH="$PATH:/usr/textbin";
 
-export GOPATH="$HOME/Projects/go"
-
+export GOPATH="$HOME/src/go:$HOME/Projects/go"
+export DOCKER_HOST="tcp://mainframe.rampant.io:2376"
+export DOCKER_TLS_VERIFY=1
