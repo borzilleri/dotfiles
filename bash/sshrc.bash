a271aa87cf41bf9c2854db0b267a80cea7d80f2d
Jonathan Borzilleri <jon.borzilleri@intrepidlearning.com>
2016-05-11 11:44:08 -0700

    Add PS1 to sshrc.bash

--- bash/sshrc.bash
+++ bash/sshrc.bash
@@ -2,4 +2,7 @@ alias l="ls -ohF --color=auto"
 export EDITOR="vim"
 export VIMINIT="let \$MYVIMRC='$SSHHOME/.sshrc.d/vimrc' | source \$MYVIMRC"
 
+export PS1="\u@\h:\W\$ "
+
 source "$SSHHOME/.sshrc.d/iterm2_shell_integration.bash"
+
