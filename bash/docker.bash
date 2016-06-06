e555d902d4b2517f0b57e6ae1219413d8d8a765b
Jonathan Borzilleri <jon.borzilleri@intrepidlearning.com>
2016-06-06 13:40:52 -0700

    Fixup docker ps script
    Fixes some errors in the join function. Print header separately, general
    cleanup

--- bash/docker.bash
+++ bash/docker.bash
@@ -1,18 +1,22 @@
 #!/bin/bash
 docker_ps_short () {
-	docker ps | awk '
-	function join(array, start, end, sep, result, i) {
+	docker ps $@ | awk '
+	function join(array, sep, result, i) {
 		if (sep == "")
 			sep = " "
 		else if (sep == SUBSEP) # magic value
 			sep = ""
-		result = array[start]
-		for (i = start + 1; i <= end; i++)
-			result = result sep array[i]
+		result = ""
+		for( i in array ) {
+			if( result != "" )
+				result = result sep
+			result = result array[i]
+		}
 		return result
 	}
 
 	function get_ports(s) {
+		port_re = "[[:digit:]]+->[[:digit:]]+/(tcp|udp)"
 		num_ports = split(s, ports_list, ",")
 		for( i in ports_list ) {
 			found = match(ports_list[i], port_re)
@@ -21,26 +25,23 @@ docker_ps_short () {
 				sub(/->/, "/", port_spec)
 				split(port_spec, port_spec_list, "/")
 				if( port_spec_list[1] != port_spec_list[2] )
-					port_spec_list[1] = port_spec_list[1] "->" port_spec_list[2] 
+					port_spec_list[1] = port_spec_list[1] "->" port_spec_list[2]
 				ports_list[i] = port_spec_list[1] "/" port_spec_list[3]
 			}
 			else
 				delete ports_list[i]
 		}
-		return join(ports_list, 1, num_ports, ",")
+		return join(ports_list, ", ")
 	}
 
-	BEGIN { FS = "  +" } ; {
-		#port_re = "([[:digit:]]\.?){4}:[[:digit:]]+->[[:digit:]]+/(tcp|udp)"
-		port_re = "[[:digit:]]+->[[:digit:]]+/(tcp|udp)"
-		id = $1; image = $2; cmd = $3; created = $4; status = $5; ports = $6; names = $7
+	BEGIN { FS = "  +" } ;
+	NR == 1 { printf "%s\t%s\t%s\t%s\n", $7, $2, $5, $6 } ;
+	NR > 1 {
+		id = $1; image = $2; cmd = $3; created = $4;
+		status = $5; ports = $6; names = $7;
 		split(status, status_a, " ")
-		if( ports == "PORTS" )
-			ports_s = ports
-		else {
-			ports_s = get_ports(ports)
-		}
+		ports_s = get_ports(ports)
 		printf "%s\t%s\t%s\t%s\n", names, image, status_a[1], ports_s
-	}' | column -t
+	}' | column -t | sort
 }
 alias dps=docker_ps_short
