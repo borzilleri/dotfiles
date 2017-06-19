406ac069f964984f80e04b4950518de2d32b623f
Jonathan Borzilleri <jon.borzilleri@intrepidlearning.com>
2017-06-19 12:17:43 -0700

    Comment out docker host, we probably dont need it.

--- bash/machine/sdf-1.bash
+++ bash/machine/sdf-1.bash
@@ -3,7 +3,7 @@ export GEM_HOME="$GEM_PATH"
 export JAVA_HOME=$(/usr/libexec/java_home)
 export MVN_HOME="$(brew --prefix maven)/libexec"
 export GOPATH="$HOME/src/go:$HOME/Projects/go"
-export DOCKER_HOST="tcp://docker.intrepid.agile:2375"
+#export DOCKER_HOST="tcp://docker.intrepid.agile:2375"
 
 PATH="$PATH:$HOME/src/go/bin"
 
