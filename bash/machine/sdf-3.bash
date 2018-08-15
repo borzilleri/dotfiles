4bfd42357ba23760207122980785a4d324e88a5c
Jonathan Borzilleri <jon.borzilleri@intrepidlearning.com>
2018-08-15 10:26:25 -0700

    Add vscode bin dir to path

--- bash/machine/sdf-3.bash
+++ bash/machine/sdf-3.bash
@@ -1,5 +1,5 @@
 export MVN_HOME="$(brew --prefix maven)/libexec"
 export GOPATH="$HOME/src/go:$HOME/Projects/go"
-PATH="$HOME/Library/Python/3.7/bin:$PATH"
+PATH="$HOME/Library/Python/3.7/bin:$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
 #complete -C aws_completer aws
