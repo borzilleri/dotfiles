20957002dd733420609b9b2b56be1da87a9c0893
Jonathan Borzilleri <jon.borzilleri@intrepidlearning.com>
2018-08-15 10:25:22 -0700

    Add local python3 bin dir to path

--- bash/machine/sdf-3.bash
+++ bash/machine/sdf-3.bash
@@ -1,10 +1,5 @@
-#export GEM_PATH="/Users/jborzilleri/.gems/1.8"
-#export GEM_HOME="$GEM_PATH"
-#export JAVA_HOME=$(/usr/libexec/java_home)
 export MVN_HOME="$(brew --prefix maven)/libexec"
 export GOPATH="$HOME/src/go:$HOME/Projects/go"
-#export DOCKER_HOST="tcp://docker.intrepid.agile:2375"
-
-#PATH="$PATH:$HOME/src/go/bin"
+PATH="$HOME/Library/Python/3.7/bin:$PATH"
 
 #complete -C aws_completer aws
