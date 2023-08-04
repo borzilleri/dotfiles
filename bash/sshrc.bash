42f4db4052e1b2c82af56f6a729fd95f1e37d675
Jonathan Borzilleri <jonathan@borzilleri.net>
2023-08-03 19:00:19 -0700

    remove unused file

--- bash/sshrc.bash
+++
@@ -1,8 +0,0 @@
-alias l="ls -ohF --color=auto"
-export EDITOR="vim"
-export VIMINIT="let \$MYVIMRC='$SSHHOME/.sshrc.d/vimrc' | source \$MYVIMRC"
-
-export PS1="\u@\h:\W\$ "
-
-source "$SSHHOME/.sshrc.d/iterm2_shell_integration.bash"
-
