ddfc286239498e7d6293e05993b9e6b7f41078d2
Jonathan Borzilleri <jon.m.borzilleri@disney.com>
2023-08-10 16:05:15 -0700

    Update disney bash file

--- bash/machine/disney.bash
+++ bash/machine/disney.bash
@@ -1,14 +1,14 @@
+include "$HOME/Projects/identity-helper-scripts/functions.bash"
+
 export MAVEN_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n
 eval "$(jenv init -)"
+exportSBT_OPTS="-Xmx2G -Xss2m"
 
 export AWSMYID_PASS="op://Disney/MyID/password"
 
 complete -C "$HOMEBREW_PREFIX/bin/aws_completer" aws
 
-export GH_HOST="github.disney.com"
-
 export GOPATH="$HOME/go"
 export GOROOT="$(brew --prefix golang)/libexec"
 export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin:${HOME}/Library/Application Support/JetBrains/Toolbox/scripts"
-
-alias ktlint="mvn antrun:run@ktlint"
+export GH_HOST="github.disney.com"
