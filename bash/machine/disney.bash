5c67588c30c6763859e40bb54dbb67365b789fb5
Jonathan Borzilleri <jon.m.borzilleri@disney.com>
2023-08-10 15:41:27 -0700

    Remove jira stuff from disney bash file

--- bash/machine/disney.bash
+++ bash/machine/disney.bash
@@ -5,14 +5,10 @@ export AWSMYID_PASS="op://Disney/MyID/password"
 
 complete -C "$HOMEBREW_PREFIX/bin/aws_completer" aws
 
-# JIRA_API_TOEN
-export JIRA_AUTH_TYPE="bearer"
 export GH_HOST="github.disney.com"
 
 export GOPATH="$HOME/go"
 export GOROOT="$(brew --prefix golang)/libexec"
 export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin:${HOME}/Library/Application Support/JetBrains/Toolbox/scripts"
-
-alias jira-work="jira issue list -a$(jira me) -s'In Progress' --plain --no-headers --columns KEY"
 
 alias ktlint="mvn antrun:run@ktlint"
