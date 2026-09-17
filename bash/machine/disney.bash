33b74491849b1029a2b1ab640530105cae1eea6d
Jonathan Borzilleri <jon.m.borzilleri@disney.com>
2026-09-17 11:46:26 -0700

    Add working directory settings to ghostty

--- bash/machine/disney.bash
+++ bash/machine/disney.bash
@@ -4,9 +4,10 @@ export SBT_OPTS="-Xmx2G -Xss2m"
 export MAVEN_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n
 eval "$(jenv init -)"
 
-
 # AWS CLI completions
 complete -C "$HOMEBREW_PREFIX/bin/aws_completer" aws
+
+eval "$(~/.local/bin/aix completion bash)"
 
 export GOPATH="$HOME/go"
 export GOROOT="$(brew --prefix golang)/libexec"
@@ -15,13 +16,9 @@ export APOLLO_TELEMETRY_DISABLED=true
 
 
 include "${SDKMAN_DIR}/bin/sdkman-init.sh"
-include "$HOME/.local/bin/pat-manager-shell/pat-manager.bash"
-# This is for the rover CLI, which is used by orchestration-registry for
-# compiling graphql schema.
-include "$HOME/.rover/env"
 
 function myid-aws() {
   op run -- awsmyid login
 }
 
-export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin:${HOME}/Library/Application Support/JetBrains/Toolbox/scripts:${HOME}/Library/Application Support/Coursier/bin:$HOME/.rover/bin"
+export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin:${HOME}/Library/Application Support/JetBrains/Toolbox/scripts:${HOME}/Library/Application Support/Coursier/bin"
