5a93efd15d5eba75e2070df99c438df6c59c52cd
Jonathan Borzilleri <jonathan@borzilleri.net>
2026-09-20 12:49:38 -0700

    Fix rc install script to refer to the proper inputrc file

--- home.rc/install.sh
+++ home.rc/install.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-ln -snF "$SRC_DIR/inputrc.bash" "$HOME/.inputrc"
-echo "Linked $HOME/.inputrc -> $SRC_DIR/inputrc.bash"
+ln -snF "$SRC_DIR/inputrc" "$HOME/.inputrc"
+echo "Linked $HOME/.inputrc -> $SRC_DIR/inputrc"
 
 ln -snF "$SRC_DIR/hushlogin" "$HOME/.hushlogin"
 echo "Linked $HOME/.hushlogin -> $SRC_DIR/hushlogin"
